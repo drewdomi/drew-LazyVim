7282c256455aaca83c360b3385daf57933bd29d3
Andrew Domingues <drewdomi@proton.me>
2023-12-23 17:09:54 -0300

    feat: add mason modules

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,10 @@ require("lazy").setup({
       colorscheme = "moonfly",
     } },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
