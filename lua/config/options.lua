24ebf219206ed536f8ed7db00c0844ded38b24f9
Andrew Domingues <drewdomi@proton.me>
2023-12-24 23:37:31 -0300

    feat: add vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.iskeyword:append("-")
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.wrap = true
+vim.opt.cursorline = false
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
