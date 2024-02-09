9ee5240d2f999c9970a267f661d733d6a19de782
Andrew Domingues <drewdomi@proton.me>
2024-02-09 09:37:17 -0300

    feat: save folding in files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,3 +3,14 @@ if vim.fn.has("nvim") == 1 then
   vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
   vim.cmd([[highlight EndOfBuffer guibg=NONE ctermbg=NONE]])
 end
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+  pattern = { "*.*" },
+  desc = "save view (folds), when closing file",
+  command = "mkview",
+})
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  pattern = { "*.*" },
+  desc = "load view (folds), when opening file",
+  command = "silent! loadview",
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,4 @@ vim.opt.cursorline = false
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.swapfile = false
+vim.opt.foldlevel = 99
